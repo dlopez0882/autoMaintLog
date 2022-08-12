5a93af5edcb7e66fd46f4477f4f0230e8b322805
David Lopez <dlopez0882@gmail.com>
2022-08-12 16:32:05 -0700

    create controller and view for vehicle page

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/vehicle/{vehicle}', [App\Http\Controllers\VehicleController::class, 'index'])->name('vehicle');
