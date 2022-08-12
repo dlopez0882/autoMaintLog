31ac6bdb9f533c424bcc39044c2f19aa4fe30513
David Lopez <dlopez0882@gmail.com>
2022-08-12 15:41:27 -0700

    change home default routing and delete laravel welcome page

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
