df4f98623b76d3bc79ef4921f8cbda73ccfe0092
David Lopez <dlopez0882@gmail.com>
2022-08-14 15:09:00 -0700

    add migration for creating work items table

---
+++ database/migrations/2022_08_14_215346_create_work_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_items', function (Blueprint $table) {
+            $table->id();
+            $table->date('service_date');
+            $table->string('vehicle_id');
+            $table->integer('mileage');
+            $table->string('services');
+            $table->decimal('cost', 7, 2);
+            $table->string('technician_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_items');
+    }
+};
