d1f92d9c769155018576bb8c7e0bf155196bcb3d
David Lopez <dlopez0882@gmail.com>
2022-08-31 16:35:39 -0700

    revise route names

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::post('/vehicles', [App\Http\Controllers\VehicleController::class, 'store'])->name('vehicle.store');
+Route::post('/vehicles', [App\Http\Controllers\VehicleController::class, 'store'])->name('vehicles.store');
 Route::get('/vehicles/{vehicle}', [App\Http\Controllers\VehicleController::class, 'show'])->name('vehicles.show');
-Route::delete('/vehicles/{vehicle}', [App\Http\Controllers\VehicleController::class, 'destroy'])->name('vehicle.destoy');
+Route::delete('/vehicles/{vehicle}', [App\Http\Controllers\VehicleController::class, 'destroy'])->name('vehicles.destoy');
 
 Route::post('/addworkitem', [App\Http\Controllers\WorkItemController::class, 'store'])->name('workitems.store');
 Route::post('/updateworkitem/{workitem}', [App\Http\Controllers\WorkItemController::class, 'update'])->name('workitems.update');
