88d321d3718d3c12141c99ec75898d2c7a594c9e
David Lopez <dlopez0882@gmail.com>
2022-09-06 18:57:42 -0700

    add FormModal and ConfirmationModal vue templates to project

---
+++ resources/js/components/ConfirmationModal.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container modal-container-confirm">
+
+        <div class="modal-header">
+          <slot name="header">
+            Confirmation modal
+          </slot>
+        </div>  
+
+        <div class="modal-body">
+          <slot name="body">
+              Are you sure that you want to delete your {{ table }}?
+          </slot>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer">
+            <form action="#" method="POST">
+              <input type="hidden" name="_token" value="csrf">
+              <input type="hidden" name="_method" :value="method">
+
+              <button type="button" class="btn btn-light me-2" @click="$emit('close')">
+                Cancel
+              </button>
+            
+              <button type="submit" class="btn btn-danger" @click="$emit('close')">
+                OK
+              </button>
+            </form>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: {
+          table: String,
+          method: String,
+      },
+      data() {
+          return {
+              //
+          }
+          
+      }
+   }
+</script>
---
+++ resources/js/components/FormModal.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+          <div class="modal-container modal-container-form">
+              <div class="card">
+                  <div class="card-header">
+                      <slot name="header">
+                          Add a new {{ headerString }}
+                      </slot>
+                  </div>  
+
+                  <div class="card-body">
+                      <slot name="body">
+                          <form action="#" method="POST">
+                              <div v-for="(type, field) in fields" class="mb-3">
+                                  <label :for="field">{{ field }}</label>
+                                  <input
+                                      :type="type" 
+                                      :name="field" 
+                                      :id="field"
+                                      class="form-control" 
+                                      value=""
+                                  >
+                                  <!-- <div class="text-danger" v-if="v$.year.$error">Year field is required and must be between {{ yearMin }} and {{ yearMax }}.</div>  -->
+                              </div>
+                              <div class="text-end">
+                                  <slot name="footer">
+                                      <button type="button" class="btn btn-light me-2" @click="$emit('close')">
+                                          Cancel
+                                      </button>
+                                      
+                                      <button type="submit" class="btn btn-primary" @click="$emit('close')">
+                                          OK
+                                      </button>
+                                  </slot>
+                              </div>
+                          </form>
+                      </slot>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: {
+          headerString: String,
+          fields: Object,
+      },
+      data() {
+          return {
+              // headerString: ''
+          }
+          
+      }
+   }
+</script>
