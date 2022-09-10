af1913b9fd7d4e5251fef4847a32a7c31256f3ae
David Lopez <dlopez0882@gmail.com>
2022-09-10 12:20:23 -0700

    creating custom error messages and clean up markup to be more readable

--- resources/js/components/FormModal.vue
+++ resources/js/components/FormModal.vue
@@ -1,146 +1,135 @@
 <template>
     <div class="modal-mask">
-      <div class="modal-wrapper">
-          <div class="modal-container modal-container-form">
-              <div class="card">
-                  <div class="card-header">
-                      <slot name="header">
-                          Add a new {{ table }}
-                      </slot>
-                  </div>  
+        <div class="modal-wrapper">
+            <div class="modal-container modal-container-form">
+                <div class="card">
+                    <div class="card-header">
+                        <slot name="header">
+                            Add a new {{ table }}
+                        </slot>
+                    </div>
 
-                  <div class="card-body">
-                      <slot name="body">
-                          <form action="submitForm" method="POST">
-                              <div v-for="field in fields" :key="field.name" class="mb-3">
-                                  <label :for="field.name">{{ field.name }}</label>
-                                  <input
-                                      :type="field.type" 
-                                      :name="field.name" 
-                                      :id="field.name"
-                                      class="form-control" 
-                                      v-model="state[field.name]"
-                                  >
-                                  <!-- <div class="text-danger" v-if="v$.year.$error">{{ field.errormsg }}.</div>   -->
-                              </div>
-                              <div class="text-danger" v-for="error of v$.$errors" :key="error.$uid">{{ error.$message }}</div> 
-                              <div class="text-end">
-                                  <slot name="footer">
-                                      <button type="button" class="btn btn-light me-2" @click="$emit('close')">
-                                          Cancel
-                                      </button>
-                                      
-                                      <button type="submit" class="btn btn-primary" @click.prevent="submitForm">
-                                          Submit
-                                      </button>
-                                  </slot>
-                              </div>
-                          </form>
-                      </slot>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+                    <div class="card-body">
+                        <slot name="body">
+                            <form action="submitForm" method="POST">
+                                <div v-for="field in fields" :key="field.name" class="mb-3">
+                                    <label :for="field.name">{{ field.name }}</label>
+                                    <input :type="field.type" :name="field.name" :id="field.name" class="form-control"
+                                        v-model="state[field.name]">
+                                    <!-- <div class="text-danger" v-if="v$.year.$error">{{ field.errormsg }}.</div>   -->
+                                </div>
+                                <div class="text-danger" v-for="error of v$.$errors" :key="error.$uid">{{ error.$message
+                                }}</div>
+                                <div class="text-end">
+                                    <slot name="footer">
+                                        <button type="button" class="btn btn-light me-2" @click="$emit('close')">
+                                            Cancel
+                                        </button>
+
+                                        <button type="submit" class="btn btn-primary" @click.prevent="submitForm">
+                                            Submit
+                                        </button>
+                                    </slot>
+                                </div>
+                            </form>
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    import { computed } from '@vue/reactivity';
+import { computed } from '@vue/reactivity';
 import useVuelidate from '@vuelidate/core';
-    import { between, required } from '@vuelidate/validators';
-    import axios from 'axios';
-    import { reactive } from 'vue';
+import { between, helpers, required } from '@vuelidate/validators';
+import axios from 'axios';
+import { reactive } from 'vue';
 
-  export default {
-      props: {
+export default {
+    props: {
         table: String,
         fields: Object,
         postroute: String,
         action: String,
-      },
-      setup(props) {
-            const state = reactive({
-                // year: '',
-                // make: '',
-                // model: ''
-            })
-            const yearMin = 1900;
-            const yearMax = new Date().getFullYear() + 1;
-            const rules = computed(() => {
-                if(props.action == 'addvehicle') {
-                    return {
-                        year: { 
-                            between: between(yearMin, yearMax),
-                            required 
-                        },
-                        make: { required },
-                        model: { required }
-                    }
-                } else {
-                    return {
-                        year: { 
-                            between: between(yearMin, yearMax),
-                            required 
-                        }
+    },
+    setup(props) {
+        const state = reactive({
+            // year: '',
+            // make: '',
+            // model: ''
+        })
+
+        const rules = computed(() => {
+            if (props.action == 'addvehicle') {
+                return {
+                    year: {
+                        between: between(1900, new Date().getFullYear() + 1),
+                        required: helpers.withMessage('Year field is required', required)
+                    },
+                    make: { required: helpers.withMessage('Make field is required', required) },
+                    model: { required: helpers.withMessage('Model field is required', required) }
+                }
+            } else {
+                return {
+                    year: {
+                        between: between(yearMin, yearMax),
+                        required: helpers.withMessage('Year field is required', required)
                     }
                 }
-            })
-            
-            const v$ = useVuelidate(rules, state)
-
-            return { state, /*requiredNameLength,*/ v$ }
-      },
-      data() {
-          return {
-            // TODO: figure out how to get field values to data properly
-          }
-          
-      },
-        methods: {
-            async submitForm () {
-                const isFormCorrect = await this.v$.$validate()
-
-                // if validation returns errors, do nothing
-                // if (!isFormCorrect) return
-                if (!isFormCorrect) {
-                    alert("errors");
-                } else {
-                    alert("success");
-                }
-
-                // otherwise, submit form
-                // gather data from form
-                // const formDataArray = this.$props.fields;
-
-                // // use proxy to extract since data is observable object (for reactivity)
-                // const myProxy = new Proxy(formDataArray, {});
-
-                // // json the data from form
-                // const myProxyTarget = JSON.parse(JSON.stringify(myProxy));
-
-                // // prepare data for axios post
-                // const data = {};
-                // for(let i = 0; i < myProxyTarget.length; i++) {
-                //     data[myProxyTarget[i].name] = myProxyTarget[i].value;
-                // }
-
-                // const config = {
-                //     headers: {
-                //         'Content-Type': 'application/json',
-                //         'X-Requested-With': 'XMLHttpRequest',
-                //         'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                //     }
-                // }
-
-                // // submit data to laravel
-                // axios.post(this.postroute, data, config)
-                //     // .then(response => console.log(response))
-                //     .then(response => {
-                //         // console.log(response)
-                //         window.location.href = '/';
-                //     })
-                //     .catch(error => console.log(error));
             }
+        })
+
+        const v$ = useVuelidate(rules, state)
+
+        return { state, /*requiredNameLength,*/ v$ }
+    },
+    methods: {
+        async submitForm() {
+            const isFormCorrect = await this.v$.$validate()
+
+            // if validation returns errors, do nothing
+            // if (!isFormCorrect) return
+            if (!isFormCorrect) {
+                alert("errors");
+            } else {
+                alert("success");
+            }
+
+            // otherwise, submit form
+            // gather data from form
+            // const formDataArray = this.$props.fields;
+
+            // // use proxy to extract since data is observable object (for reactivity)
+            // const myProxy = new Proxy(formDataArray, {});
+
+            // // json the data from form
+            // const myProxyTarget = JSON.parse(JSON.stringify(myProxy));
+
+            // // prepare data for axios post
+            // const data = {};
+            // for(let i = 0; i < myProxyTarget.length; i++) {
+            //     data[myProxyTarget[i].name] = myProxyTarget[i].value;
+            // }
+
+            // const config = {
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'X-Requested-With': 'XMLHttpRequest',
+            //         'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            //     }
+            // }
+
+            // // submit data to laravel
+            // axios.post(this.postroute, data, config)
+            //     // .then(response => console.log(response))
+            //     .then(response => {
+            //         // console.log(response)
+            //         window.location.href = '/';
+            //     })
+            //     .catch(error => console.log(error));
         }
-   }
+    }
+}
 </script>
