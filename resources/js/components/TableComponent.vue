744558ade2f634de46dfb5294ad94b9d9d2f5ef5
David Lopez <dlopez0882@gmail.com>
2022-09-10 13:33:54 -0700

    code clean up

--- resources/js/components/TableComponent.vue
+++ resources/js/components/TableComponent.vue
@@ -1,179 +1,100 @@
 <template>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">My {{ tableName }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">My {{ tableName }}</div>
 
-                <div class="card-body">
-                    <!-- @if($vehicles->count() == 0) -->
-                    <div v-if="!items.length" class="mb-2">No {{ tableName }} found!</div>
+                    <div class="card-body">
+                        <div v-if="!items.length" class="mb-2">No {{ tableName }} found!</div>
 
-                    <!-- @else -->
-                    <table class="table table-striped task-table">
-                        <!-- Table Headings -->
-                        <thead>
-                            <tr>
-                                <!-- <th v-for="(column, index) in columns" :key="index">{{ column }}</th> -->
-                                <th v-for="column in columns">{{ column }}</th>
-                                <th>&nbsp;</th>
-                                <th>&nbsp;</th>
-                            </tr>
-                            
-                        </thead>
-    
-                        <!-- Table Body -->
-                        <tbody>
-                            <!-- @foreach ($vehicles as $vehicle)
+                        <table class="table table-striped task-table">
+                            <!-- Table Headings -->
+                            <thead>
                                 <tr>
-                                    <td class="table-text">
-                                        <div>{{ $vehicle->year . " " . $vehicle->make . " " . $vehicle->model }}</div>
-                                    </td> -->
+                                    <th v-for="column in columns">{{ column }}</th>
+                                    <th>&nbsp;</th>
+                                    <th>&nbsp;</th>
+                                </tr>
+                            </thead>
+
+                            <!-- Table Body -->
+                            <tbody>
                                 <tr v-for="item in items" :key="item.id">
                                     <td class="table-text" v-for="column in columns">{{ item[column] }}</td>
-
-
-                                    <!-- Show records button button -->
-                                    <!-- <td>
-                                        <a href="{{ url("/vehicles/{$vehicle->id}") }}" class="btn btn-primary" title="See Service History"><i class="fa fa-gear"></i> See Service History</a>
-                                    </td> -->
-    
-                                    <!-- Button to open delete confirmation modal -->
-                                    <!-- <td>
-                                        <button 
-                                            id="show-modal" 
-                                            class="btn btn-danger" 
-                                            title="Delete Vehicle"
-                                            @click="showModal = true, vehicle={{ json_encode($vehicle) }}, postroute='{{ url("/vehicles/{$vehicle->id}") }}'"
-                                            @keydown.esc="showModal = false" 
-                                        >
-                                        <i class="fa fa-trash"></i> Delete Vehicle</button>
-                                    </td> -->
-
                                     <td v-for="option in options">
-                                        <a v-if="option == 'view'" :href="tableName + '/' + item.id" class="btn btn-primary" title="view record">View {{item.id}}</a>
-                                        <!-- <button v-else-if="option == 'delete'" type="button" class="btn btn-danger" title="delete record">Delete</button> -->
+                                        <a v-if="option == 'view'" :href="tableName + '/' + item.id"
+                                            class="btn btn-primary" title="view record">View {{item.id}}</a>
 
-                                        <button v-else-if="option == 'delete'" @click="showConfirmationModal()" type="button" class="btn btn-danger" title="delete record">Delete</button>
+                                        <button v-else-if="option == 'delete'" @click="showConfirmationModal()"
+                                            type="button" class="btn btn-danger" title="delete record">Delete</button>
                                     </td>
-
                                 </tr>
-                            <!-- @endforeach -->
-                        </tbody>
-                    </table>
-                    <!-- @endif -->
-                    
-                    <!-- <button 
-                        type="button"
-                        id="show-add-vehicle-modal"
-                        class="btn btn-primary"
-                        title="Add Vehicle"
-                        @click="showAddVehicleModal = true, postroute='{{ url("/vehicles") }}'"
-                        @keydown.esc="showAddVehicleModal = false" 
-                    > -->
-                    <!-- <button
-                        type="button"
-                        class="btn btn-primary"
-                        title="Add"
-                        @click="showAddVehicleModal = true, postroute=tableName + '/'"
-                    ><i class="fa fa-plus"></i> Add {{ tableName }}</button> -->
+                            </tbody>
+                        </table>
 
-                    <button
-                        type="button"
-                        class="btn btn-primary"
-                        :title="`Add ${tableName.slice(0,-1)}`"
-                        @click="showFormModal(`/${tableName}`, `add${tableName.slice(0,-1)}`)"
-                    ><i class="fa fa-plus"></i> Add {{ tableName.slice(0,-1) }}</button>
+                        <button type="button" class="btn btn-primary" :title="`Add ${tableName.slice(0,-1)}`"
+                            @click="showFormModal(`/${tableName}`, `add${tableName.slice(0,-1)}`)"><i
+                                class="fa fa-plus"></i> Add {{ tableName.slice(0,-1) }}</button>
 
+                    </div>
                 </div>
             </div>
         </div>
+
+        <transition name="modal">
+            <ConfirmationModal v-if="displayConfirmationModal" @close="closeConfirmationModal" :table="tableName"
+                :method="'delete'"></ConfirmationModal>
+
+            <FormModal v-else-if="displayFormModal" @close="closeFormModal" :table="tableName" :fields="fields"
+                :postroute="postroute" :action="action"></FormModal>
+        </transition>
+
     </div>
-
-    <transition name="modal">
-        <!-- <add-vehicle-modal-component 
-            v-if="showAddVehicleModal" 
-            @close="showAddVehicleModal = false" 
-            v-bind:data = "{postroute:postroute}"
-        >
-        </add-vehicle-modal-component> -->
-
-        <!-- <component :is=currentComponent
-        v-if="showAddVehicleModal" 
-            @close="showAddVehicleModal = false" 
-            v-bind:data = "{postroute:postroute}"
-        ></component> -->
-
-        <ConfirmationModal
-            v-if="displayConfirmationModal"
-            @close="closeConfirmationModal"
-            :table="tableName"
-            :method="'delete'"
-        ></ConfirmationModal>
-
-        <FormModal
-            v-else-if="displayFormModal"
-            @close="closeFormModal"
-            :table="tableName"
-            :fields="fields"
-            :postroute="postroute"
-            :action="action"
-        ></FormModal>
-    </transition>
-
-</div>
 </template>
 
 
 
 <script>
-    import ConfirmationModal from './ConfirmationModal.vue';
-    import FormModal from './FormModal.vue';
+import ConfirmationModal from './ConfirmationModal.vue';
+import FormModal from './FormModal.vue';
 
-    export default {
-        data() {
-            return {
-                // showAddVehicleModal: false,
-                // currentComponent: this.$props.inputmodal,
-
-                displayConfirmationModal: false,
-                displayFormModal: false,
-                // fields: ''
-                postroute: '',
-                action: '',
-            };
+export default {
+    data() {
+        return {
+            displayConfirmationModal: false,
+            displayFormModal: false,
+            postroute: '',
+            action: '',
+        };
+    },
+    props: {
+        items: Object,
+        tableName: [String, Number],
+        columns: Array,
+        options: Array,
+        fields: Array,
+    },
+    methods: {
+        showConfirmationModal() {
+            this.displayConfirmationModal = true;
         },
-        props: {
-            items: Object,
-            tableName: [String, Number],
-            columns: Array,
-            options: Array,
-            // inputmodal: String,
-            fields: Array,
+        closeConfirmationModal() {
+            this.displayConfirmationModal = false;
         },
-        methods: {
-            showConfirmationModal() {
-                this.displayConfirmationModal = true;
-            },
-            closeConfirmationModal() {
-                this.displayConfirmationModal = false;
-            },
-            showFormModal(postroute, action) {
-                this.displayFormModal = true;
-                // this.fields = fields;
-                this.postroute = postroute;
-                this.action = action;
-            },
-            closeFormModal() {
-                this.displayFormModal = false;
-                // this.fields = '';
-                this.postroute = '';
-            }
+        showFormModal(postroute, action) {
+            this.displayFormModal = true;
+            this.postroute = postroute;
+            this.action = action;
         },
-        mounted() {
-            console.log("Table component mounted");
-            // console.log(this.$props.inputmodal);
-        },
-        components: { ConfirmationModal, FormModal }
-    }
+        closeFormModal() {
+            this.displayFormModal = false;
+            this.postroute = '';
+        }
+    },
+    mounted() {
+        console.log("Table component mounted");
+    },
+    components: { ConfirmationModal, FormModal }
+}
 </script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,89 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('My Vehicles') }}</div>
-
-                <div class="card-body">
-                    @if($vehicles->count() == 0)
-                    <div class="mb-2">No vehicles found!</div>
-                    @else
-                    <table class="table table-striped task-table">
-                        <!-- Table Headings -->
-                        <thead>
-                            <th>Vehicle</th>
-                            <th>&nbsp;</th>
-                            <th>&nbsp;</th>
-                        </thead>
-    
-                        <!-- Table Body -->
-                        <tbody>
-                            @foreach ($vehicles as $vehicle)
-                                <tr>
-                                    <td class="table-text">
-                                        <div>{{ $vehicle->year . " " . $vehicle->make . " " . $vehicle->model }}</div>
-                                    </td>
-                                    
-                                    <td>
-                                        <!-- Show records button button -->
-                                        <a href="{{ url("/vehicles/{$vehicle->id}") }}" class="btn btn-primary" title="See Service History"><i class="fa fa-gear"></i> See Service History</a>
-                                    </td>
-    
-                                    <td>
-                                        <!-- Button to open delete confirmation modal -->
-                                        <button 
-                                            id="show-modal" 
-                                            class="btn btn-danger" 
-                                            title="Delete Vehicle"
-                                            @click="showModal = true, vehicle={{ json_encode($vehicle) }}, postroute='{{ url("/vehicles/{$vehicle->id}") }}'"
-                                            @keydown.esc="showModal = false" 
-                                        >
-                                        <i class="fa fa-trash"></i> Delete Vehicle</button>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    @endif
-                    
-                    <button 
-                        type="button"
-                        id="show-add-vehicle-modal"
-                        class="btn btn-primary"
-                        title="Add Vehicle"
-                        @click="showAddVehicleModal = true, postroute='{{ url("/vehicles") }}'"
-                        @keydown.esc="showAddVehicleModal = false" 
-                    >
-                    <i class="fa fa-plus"></i> Add Vehicle</button>
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <transition name="modal">
-        <modal-component v-if="showModal" @close="showModal = false" v-bind:vehicledata = "{vehicle:vehicle, postroute:postroute}">
-            <template v-slot:header>
-                <h3>Confirm</h3>
-            </template>
-        </modal-component>
-    </transition>
-
-    <!-- <transition name="modal">
-        <add-vehicle-modal-component v-if="showAddVehicleModal" @close="showAddVehicleModal = false" v-bind:data = "{postroute:postroute}"></add-vehicle-modal-component>
-    </transition> -->
-
-</div> --}}
 
 <table-component 
     :items="{{ $vehicles }}" 
     :table-name="'vehicles'" 
     :columns="['year', 'make', 'model']"
     :options="['view', 'delete']"
-    {{-- :inputmodal="'add-vehicle-modal-component'" --}}
     :fields="[    
         {'name': 'year', 'type': 'number'},
         {'name': 'make', 'type': 'text'},
