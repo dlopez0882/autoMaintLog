9e8a5a53c3f8eae1d72495d24db57a3758e09048
David Lopez <dlopez0882@gmail.com>
2022-09-14 12:32:38 -0700

    update post request for creating a vehicle record

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -55,9 +55,9 @@ class VehicleController extends Controller
     public function store(Request $request)
     {
         $request->user()->vehicles()->create([
-            'year' => $request['year'],
-            'make' => $request['make'],
-            'model' => $request['model'],
+            'year' => $request->formData['year'],
+            'make' => $request->formData['make'],
+            'model' => $request->formData['model'],
         ]);
      
         return;
