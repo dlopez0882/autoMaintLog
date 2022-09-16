c437dd7fb6c3f0b960b44ab2a63a158a98906422
David Lopez <dlopez0882@gmail.com>
2022-09-16 00:10:07 -0700

    bug fix for displaying tinymce content

--- resources/js/components/DetailsBody.vue
+++ resources/js/components/DetailsBody.vue
@@ -2,7 +2,7 @@
 <!-- method in v-for loop may run twice. see https://github.com/vuejs/vue/issues/10558#issuecomment-534967376 -->
 <template>
     <div v-for="field in fields" class="mb-3">{{ uppercaseFirstLetterAndRemoveUnderscores(field.name) }}:
-        <div v-if="details[field.type] == 'tinymce'" v-html="details[field.name]"></div>
+        <div v-if="field.type == 'tinymce'" v-html="details[field.name]"></div>
         <div v-else>{{ numberFormatter(details[field.name], field.format) }}</div>
     </div>
 </template>
